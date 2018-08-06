ae0de9a4c83774e5864007360e26f6032fe310a3
Leo Bartkus <leo.bartkus@gmail.com>
2018-08-06 16:28:01 -0500

    character animation updates
    walk animation
    move idles to triggers
    rework the pyxel file into layers

--- Game2/Assets/Scripts/PlayerBehavior.cs
+++ Game2/Assets/Scripts/PlayerBehavior.cs
@@ -6,13 +6,44 @@ public class PlayerBehavior : MonoBehaviour {
 
 	public float Speed = 1.0f;
 
+	public float IdleTime = 5.0f;
+	public bool IsMoving = false;
+
+	public bool IsMovingRight = false;
+	public bool IsMovingLeft = false;
+
+	private float LastInputTime = 0f;
+
 	// Update is called once per frame
 	public void Update () {
-		
-        var moveY = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-        var moveX = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
 
-		this.transform.position += new Vector3(moveX, moveY, 0);
+		this.LastInputTime += Time.deltaTime;
+
+		var animator = this.GetComponent<Animator>();
+
+        float moveY = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        float moveX = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+
+		Vector3 moveDir = new Vector3(moveX, moveY, 0);
+
+		if(moveDir.x < 0){
+			this.transform.localScale = new Vector3(-1, 1, 1);
+		}
+
+		if(moveDir.x > 0) {
+			this.transform.localScale = new Vector3(1, 1, 1);
+		}
+
+		this.IsMoving = moveDir.magnitude > 0;
+
+		animator.SetBool("IsMoving", this.IsMoving);
+
+		if(this.LastInputTime > this.IdleTime){
+			animator.SetTrigger("Idle" + Random.Range(1, 3).ToString());
+			this.LastInputTime = 0;
+		}
+
+		this.transform.position += moveDir;
 
 	}
 }
