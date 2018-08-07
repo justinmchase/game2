d1b60b9d9fc6715ed23ff0c8bbabc9181151ceff
Leo Bartkus <leo.bartkus@gmail.com>
2018-08-07 14:32:27 -0500

    more character animation
    move up and move down animations
    move "random" idle into a blend tree

--- Game2/Assets/Scripts/PlayerBehavior.cs
+++ Game2/Assets/Scripts/PlayerBehavior.cs
@@ -6,25 +6,29 @@ public class PlayerBehavior : MonoBehaviour {
 
 	public float Speed = 1.0f;
 
-	public float IdleTime = 5.0f;
+	public float IdleTime = 0.0f;
 	public bool IsMoving = false;
 
 	public bool IsMovingRight = false;
 	public bool IsMovingLeft = false;
 
-	private float LastInputTime = 0f;
-
 	// Update is called once per frame
 	public void Update () {
 
-		this.LastInputTime += Time.deltaTime;
+		this.IdleTime += Time.deltaTime;
 
 		var animator = this.GetComponent<Animator>();
 
         float moveY = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
         float moveX = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
 
-		Vector3 moveDir = new Vector3(moveX, moveY, 0);
+		Vector3 velocity = new Vector3(moveX, moveY, 0);
+
+		Vector3 moveDir = velocity;
+		if(moveDir.magnitude != 0){
+			moveDir.Normalize();
+			this.IdleTime = 0;
+		}
 
 		if(moveDir.x < 0){
 			this.transform.localScale = new Vector3(-1, 1, 1);
@@ -34,16 +38,15 @@ public class PlayerBehavior : MonoBehaviour {
 			this.transform.localScale = new Vector3(1, 1, 1);
 		}
 
-		this.IsMoving = moveDir.magnitude > 0;
+		animator.SetFloat("MoveX", moveDir.x);
+		animator.SetFloat("MoveY", moveDir.y);
 
+		this.IsMoving = moveDir.magnitude > 0;
 		animator.SetBool("IsMoving", this.IsMoving);
 
-		if(this.LastInputTime > this.IdleTime){
-			animator.SetTrigger("Idle" + Random.Range(1, 3).ToString());
-			this.LastInputTime = 0;
-		}
-
-		this.transform.position += moveDir;
+		animator.SetFloat("IdleTime", this.IdleTime);
+		
+		this.transform.position += velocity;
 
 	}
 }
