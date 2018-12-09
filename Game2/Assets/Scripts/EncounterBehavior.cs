e1ed27680dd74a00483d2b77eceb5cb914734e0c
Justin Chase <justin.m.chase@gmail.com>
2018-12-09 11:30:36 -0600

    Random encounters

---
+++ Game2/Assets/Scripts/EncounterBehavior.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable()]
+public class EncounterPossibility {
+  public float Probibility;
+  public GameObject prefab;
+}
+
+public class EncounterBehavior : MonoBehaviour {
+  public EncounterPossibility[] Possibilities;
+
+  public void Start()
+  {
+    var rand = new System.Random();
+    var prob = rand.NextDouble();
+    var last = 0.0d;
+
+    for (var i = 0; i < this.Possibilities.Length; i++) {
+      var p = this.Possibilities[i];
+      if (prob < last + p.Probibility) {
+        Debug.Log("Encounter!");
+        var obj = GameObject.Instantiate(p.prefab);
+        obj.transform.parent = this.transform;
+        obj.transform.position  = this.transform.position;
+        break;
+      } else {
+        last += p.Probibility;
+      }
+    }
+  }
+}
